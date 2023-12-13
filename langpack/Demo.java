e34cd20fc6c242e9f85b4368d080f8596da08a19
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-12-13 12:37:56 +0530

    test

---
+++ langpack/Demo.java
@@ -0,0 +1,8 @@
+//import java.lang.String;
+import java.util.ArrayList;
+class Demo{
+    public static void main(String[] args){
+        new String("Rahul");
+        new ArrayList(); 
+    }
+}
---
+++ langpack/Test.java
@@ -0,0 +1,10 @@
+import java.lang.Object;
+
+class Test extends Object{
+   
+    public static void main(String[] args){
+        Test t1=new Test();
+        System.out.println(t1);
+        System.out.println(t1.toString());
+    }
+}
